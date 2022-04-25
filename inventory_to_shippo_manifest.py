71200a14a72c3b96684c12a86754e55c40ecf345
Raman A. Shah <raman.anand.shah@gmail.com>
2022-04-25 13:14:53 -0400

    inventory_to_shippo_manifest.py: size to weight logic.
    Enter empirical mean weights of t-shirts and implement logic to
    translate shirt sizes into postal weights with some margin of safety.
    
    Close #2.

--- inventory_to_shippo_manifest.py
+++ inventory_to_shippo_manifest.py
@@ -3,13 +3,44 @@
 
 import copy
 import csv
+import math
 import re
 from sys import argv
 import warnings
 
 import openpyxl
 
-# Hard-code a 9 oz package sent to USA addresses
+# Measured masses of t-shirts by size, in oz
+SHIRT_WEIGHTS = {'MXS': 3.45 / 1,
+                 'MS': 11.45 / 3,
+                 'MM': 38.80 / 9,
+                 'ML': 33.35 / 7,
+                 'MXL': 21.15 / 4,
+                 'M2XL': 17.60 / 3,
+                 'M3XL': 6.55 / 1,
+                 'WS': 9.75 / 3,
+                 'WM': 18.05 / 5,
+                 'WL': 16.55 / 4,
+                 'WXL': 17.95 / 4,
+                 'W2XL': 9.95 / 2}
+
+# Empirical, based on finished packages:
+# MM: 7.65 oz
+# ML: 8.45 oz
+BALANCE_OF_SHIPMENT = \
+    ((7.65 + 8.45) - (SHIRT_WEIGHTS['MM'] + SHIRT_WEIGHTS['ML'])) / 2.0 # ~3.51 oz
+
+# Shipping weights are actually uncertain due to manufacturing variance and
+# taping technique. To demonstrate the uncertainty, I weighed a box (3.15 oz)
+# and card with envelope (0.40 oz) and the sum (3.55 oz) is more than the
+# empirical balance of shipment, which includes a label and tape. I saw
+# individual shirt weights vary ~0.5 oz min to max. Add a small margin of
+# safety to reduce the risk of buying too little postage (noting that having
+# too much margin jacks typical MM orders, which weigh close to 8 oz, from 8 oz
+# to 9 oz, which incurs a big price increase).
+MARGIN_OF_SAFETY = 0.15
+
+# Hard-code USA addresses
 SHIPPO_FIELDS = {'Order Number': None,
                  'Order Date': None,
                  'Recipient Name': None,
@@ -30,15 +61,15 @@ SHIPPO_FIELDS = {'Order Number': None,
                  'Item Weight Unit': None,
                  'Item Price': None,
                  'Item Currency': None,
-                 'Order Weight': 9,
+                 'Order Weight': None,
                  'Order Weight Unit': 'oz',
                  'Order Amount': None,
                  'Order Currency': None}
 
 STATE_AND_ZIP = re.compile(r'(.*) ([A-Z]{2}),? ([0-9]{5})$')
 
-def address_cell_to_shippo_details(name_text, address_text):
-    '''Parse my comma-separated addresses into Shippo fields.'''
+def shippo_details(size_text, name_text, address_text):
+    '''Parse spreadsheet row into Shippo fields.'''
     result = copy.deepcopy(SHIPPO_FIELDS)
 
     # Real addresses and not notes to hand-deliver have parts, thus commas
@@ -49,16 +80,10 @@ def address_cell_to_shippo_details(name_text, address_text):
             result['State/Province'] = state_and_zip_match.group(2)
             result['Zip/Postal Code'] = state_and_zip_match.group(3)
 
-            # TODO Look up postage weight as a function of shirt size.
-            # Empirically, measuring finished packages with a card, tape, and label:
-            # MM: 7.65 oz
-            # ML: 8.45 oz
-            # It will save some pennies to buy <= 8 oz of postage when
-            # applicable, and >= XL shirts may require >= 10 oz. (The biggest
-            # shirts probably won't fit in the standard packaging and will
-            # require a Flat Rate Box or similar.) To do this, easiest will
-            # probably be to measure the shirt weights and extrapolate the
-            # packed weight.
+            result['Order Weight'] = math.ceil(
+                SHIRT_WEIGHTS[size_text] +
+                BALANCE_OF_SHIPMENT +
+                MARGIN_OF_SAFETY)
 
             address_city = state_and_zip_match.group(1).strip(',').split(', ')
             result['City'] = address_city[-1]
@@ -102,12 +127,16 @@ assert wb['outgoing']['E1'].value == 'shipped'
 ir = wb['outgoing'].iter_rows()
 next(ir)  # Header row
 for row in ir:
+    size = row[1].value
+    if '2XL' in size or '3XL' in size:
+        warnings.warn('Shirts bigger than XL may need a bigger box.')
+
     name = row[2].value
     address = row[3].value
     shipped = row[4].value
 
     if shipped != 'Y':
-        fields = address_cell_to_shippo_details(name, address)
+        fields = shippo_details(size, name, address)
 
         if fields is not None:
             results.append(fields)
